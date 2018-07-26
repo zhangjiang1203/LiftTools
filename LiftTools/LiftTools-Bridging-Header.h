db25d02dfed42401e4f8af5ad184fea34a0558b8
zhangjiang1203 <896884553@qq.com>
2018-07-26 16:18:34 +0800

    修改头文件

--- LiftTools/LiftTools-Bridging-Header.h
+++ LiftTools/LiftTools-Bridging-Header.h
@@ -2,22 +2,11 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-//#import "MJRefresh.h"
-#ifndef SwiftBaseProject_Bridging_Header_h
-#define SwiftBaseProject_Bridging_Header_h
+//头文件的引用
 
-
-//#import "UIImageView+CornerRadius.h"
 #import "MBProgressHUD.h"
 #import "MBProgressHUD+Add.h"
-//#import <SDWebImage/UIImageView+WebCache.h>
 #import <MJRefresh/MJRefresh.h>
 #import <SVProgressHUD/SVProgressHUD.h>
 #import <iCarousel/iCarousel.h>
 #import <ZFPlayer/ZFPlayer.h>
-
-//#import "iCarousel.h"
-//#import <YYWebImage/YYWebImage.h>
-
-
-#endif
